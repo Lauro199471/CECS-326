a6b305acc94f1a8b77f50d7676c0856dd7dda196
Lauro Cabral <lauro199471@gmail.com>
2018-01-27 01:30:21 -0800

    Updated C Files

--- Assignment/Assignment 1/Assignment_1/main.cpp
+++ Assignment/Assignment 1/Assignment_1/main.cpp
@@ -6,28 +6,108 @@
 
 void initHeader(void)
 {
-    printf(yellow"\t  CECS 326\n"reset);
-    printf(gray"\tLauro Cabral\n"reset);
+    printf(yellow "\t\t\t\t\t\t  CECS 326\n" reset);
+    printf(gray "\t\t\t\t\t\tLauro Cabral\n" reset);
+}
+void menuStart()
+{
+  printf(red "(1)" reset " Access a Pointer"
+         green "  (2)" reset " List Deallocated Memory(Index)"
+         blue  "  (3)" reset " Deallocate All Memory"
+         yellow "  (4)" reset " Exit Program\n>> ");
+}
+
+int menu1()
+{
+  int x;
+  printf(red"Index: " reset);
+  scanf ("%d", x);
+  return x;
+}
+void menu1_2(void)
+{
+    printf(red "(1)" reset " Print Char's"
+           green "  (2)" reset " Delete All Chars"
+           blue  "  (3)" reset " Return to Main Menu\n>>"
+          );
 }
 
 int main()
 {
+   int* exp;
+   int i , index;
+   int userInput = 0;
 
    myStruct mainStruct;
    srand(time(0));
 
-   initHeader();
-
    init_INT_ARRAY(&mainStruct);
+
+   while(userInput != 4)
+   {
+       system("clear");
+       initHeader();
+       menuStart();
+       scanf ("%d",&userInput);
+
+       if(userInput == 1)
+       {
+            system("clear");
+            initHeader();
+            index = menu1();
+            userInput = 0;
+            while(userInput != 3)
+            {
+                menu1_2();
+                scanf ("%d",&userInput);
+                if(userInput == 3)
+                    break;
+                if(userInput == 2)
+                    deallocateAMemory(&mainStruct , index);
+                if(userInput == 1)
+                    print_CHARPTR_ARRAY(&mainStruct);
+            }
+       }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+   //////////////////////////////////////////////////////////////
+   /*
    print_INT_ARRAY(&mainStruct);
 
    printf("\n");
 
    init_CHARPTR_ARRAY(&mainStruct);
-   print_CHARPTR_ARRAY(&mainStruct);
+   print_CHARPTR_ARRAY_HEAP(&mainStruct);
 
+   printf("\n");
+   //deallocateAllMemory(&mainStruct);
+     deallocateAMemory(&mainStruct , 3);
+
+   //system("clear");
+   initHeader();
+
+   if(mainStruct.charArray[3] == NULL)
+   {
+        printf(green "\nPass\n");
+   }
+   else
+   {
+        printf(red "\nNo Pass\n");
+   }
 
    printf(reset "DONE\n");
-
+   */
+   printf(reset "\n");
    return 0;
 }
--- Assignment/Assignment 1/Assignment_1/supportassignment1.h
+++ Assignment/Assignment 1/Assignment_1/supportassignment1.h
@@ -25,7 +25,7 @@ void init_CHARPTR_ARRAY(myStruct* c)
     int i;
     for(i = 0; i < 20 ; i = i + 1)
     {
-        c->charArray[i] = new char[c->intArray[i]];
+        c->charArray[i] = (char*)malloc(c->intArray[i]);
         c->charArray[i][0] = (rand()%25)+65;
     }
 }
@@ -33,14 +33,16 @@ void init_CHARPTR_ARRAY(myStruct* c)
 void print_INT_ARRAY(myStruct *c)
 {
     int i;
-    for(i=0;i<18;i=i+3)
+    for(i=0;i<20;i=i+4)
     {
       printf("[" red "%X" reset "]: " blue "%10d  " reset,
              &(c->intArray[i]) , c->intArray[i]);
       printf("[" red "%X" reset "]: " blue "%10d  " reset,
              &(c->intArray[i+1]) , c->intArray[i+1]);
-      printf("[" red "%X" reset "]: " blue "%10d\n" reset,
+      printf("[" red "%X" reset "]: " blue "%10d  " reset,
              &(c->intArray[i+2]) , c->intArray[i+2]);
+      printf("[" red "%X" reset "]: " blue "%10d\n" reset,
+             &(c->intArray[i+3]) , c->intArray[i+3]);
 
     }
 }
@@ -48,15 +50,35 @@ void print_INT_ARRAY(myStruct *c)
 void print_CHARPTR_ARRAY(myStruct *c)
 {
     int i;
-    for(i=0;i<18;i=i+3)
+    for(i=0;i<20;i=i+4)
     {
-        printf("[" green "%9X" reset "]: " magenta "%1c  " reset,
-              &(c->charArray[i][0]) , c->charArray[i][0]);
-        printf("[" green "%9X" reset "]: " magenta "%1c  " reset,
-              &(c->charArray[i+1][0]) , c->charArray[i+1][0]);
-        printf("[" green "%9X" reset "]: " magenta "%1c\n" reset,
-              &(c->charArray[i+2][0]) , c->charArray[i+2][0]);
+        printf(yellow "%2d" reset ":[" green "%9X" reset "]: " magenta "%1c  " reset,
+              i + 1 , c->charArray[i] , c->charArray[i][0]);
+        printf(yellow "%2d" reset ":[" green "%9X" reset "]: " magenta "%1c  " reset,
+              i + 2 , &(c->charArray[i+1][0]) , c->charArray[i+1][0]);
+        printf(yellow "%2d" reset ":[" green "%9X" reset "]: " magenta "%1c  " reset,
+              i + 3 , &(c->charArray[i+2][0]) , c->charArray[i+2][0]);
+        printf(yellow "%2d" reset ":[" green "%9X" reset "]: " magenta "%1c\n" reset,
+              i + 4 , &(c->charArray[i+3][0]) , c->charArray[i+3][0]);
     }
+}
+
+
+
+void deallocateAllMemory(myStruct* c)
+{
+    int i;
+    for(i=0;i<20;i=i+1)
+    {
+        free((*c).charArray[i]);
+        c->charArray[i] = NULL;
+    }
+}
+
+void deallocateAMemory(myStruct* c ,int i)
+{
+    free((*c).charArray[i]);
+    c->charArray[i] = NULL;
 }
 #endif // SUPPORTASSIGNMENT1_H
 
