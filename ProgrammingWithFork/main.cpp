6f36220456e87c97ff349f167657bcd4addcb113
Lauro Cabral <lauro199471@gmail.com>
2018-03-25 21:24:06 -0700

    added replace

--- ProgrammingWithFork/main.cpp
+++ ProgrammingWithFork/main.cpp
@@ -8,22 +8,38 @@ using namespace std;
 int main() {
   string search;
   string candidate ;
+  string replace;
   int countwords = 0 ;
     
   ifstream File("robot.txt");
+  ofstream fileout("temp.txt"); //Temporary file
+  
+  if(!File || !fileout) //if both files are not available
+  {
+    cout << "Error opening files!" << endl;
+    return 1;
+  }
   if(File.is_open())
   {
-    cout << "File opened.\n" ;
-
-    cout << "Write the word you're searching for\n" ;
+    cout << "Write the word you're searching for:\033[1;32m ";
     cin >> search ;
-
+    cout << "\033[0m";
+    
+    cout << "Replace:\033[1;31m ";
+    cin >> replace;
+    cout << "\033[0m";
+    
     while(File>>candidate ) 
     {
-      if( search == candidate ) ++countwords ;
+      if( search == candidate ) //if your word found then replace
+      {
+        candidate = replace;
+        ++countwords ;  
+      } 
+      fileout << candidate << " ";
     }
 
-    std::cout << "The word '" << search << "' has been found " << countwords << " times.\n" ;
+    cout << "The word '" << search << "' has been found " << countwords << " times.\n" ;
   }
 
   else
