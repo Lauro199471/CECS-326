16aa0a1d51fd1356aab9ac06a9eaee639ad3e7ba
Lauro Cabral <lauro199471@gmail.com>
2018-03-26 23:26:50 -0700

    Finished

--- ProgrammingWithFork/main.cpp
+++ ProgrammingWithFork/main.cpp
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <bits/signum.h>
 
 #define running 1
 
@@ -31,7 +32,7 @@ int main() {
   
   if(filein.is_open() && temp_fileout.is_open() && temp_filein.is_open())
   {
-    cout << "Files are good\n";
+    cout << "================\n|Files are good|\n================\n\n";
   }
   
   // clear temp file
@@ -49,40 +50,49 @@ int main() {
   temp_fileout.close();
   temp_filein.close();
   
+
   while(running)
   {
     countwords = 0; textTotal = "";status = 0;pid=1;
-    
+    printf("\n\n-- beginning of Parent program --\n");
     // Get User input        
-    cout << "\n\nWrite the word you're searching for:\033[1;32m ";
+    cout << "\nWrite the word you're searching for:\033[1;32m ";
     cin >> search ;
     cout << "\033[0m";
 
     // if string is '!wq' terminate
     if(search.compare("!wq") == 0)
     {
-        cout << "\n--End of Program--\n";
-        // wait for child to finish
-        wait(NULL);
-        wait(NULL);
-        break;
-
+      cout << "\n-- End of Program --\n";
+      exit(EXIT_SUCCESS);
     }
 
     cout << "Replace:\033[1;31m ";
     cin >> replace;
     cout << "\033[0m\n";
     
-    // Find and replace using child process
-    printf("\n--beginning of Child program--\n");
-    pid = fork();
-   
-    if(pid == 0)
+    // Print what words are going to be replaced
+    temp_filein.open("temp.txt");
+    while(temp_filein>>candidate ) 
     {
-        // Child Process
-        temp_filein.open("temp.txt");
-        while(temp_filein>>candidate ) 
-        {
+      if( search == candidate ) //if your word found then replace
+        candidate = "\033[1;32m"+search+"\033[0m";
+      textTotal = textTotal + " " + candidate;
+    }
+    cout << textTotal << endl;
+    temp_filein.close();
+    countwords = 0; textTotal = "";status = 0;pid=1;
+    
+    // Find and replace using child process
+    printf("\n--beginning of Child program--\n\n");
+    pid = fork();// Create child process
+    
+    // Child Process
+    if(pid == 0)
+    { 
+      temp_filein.open("temp.txt");
+      while(temp_filein>>candidate ) 
+      {
         text = candidate;
         if( search == candidate ) //if your word found then replace
         {
@@ -92,37 +102,35 @@ int main() {
         } 
         textTotal = textTotal + " " + text;
         cout << candidate << " "; 
-        }
-        temp_filein.close();
+      }
+      
+      temp_filein.close();
+      // Clear and Fill
+      temp_fileout.open("temp.txt",ofstream::out | ofstream::trunc);
+      temp_fileout.close();
+      temp_fileout.open("temp.txt");
 
-        // Clear and Fill
-        temp_fileout.open("temp.txt",ofstream::out | ofstream::trunc);
-        temp_fileout.close();
-        temp_fileout.open("temp.txt");
-
-
-        temp_fileout << textTotal;
-        cout << "\n\nThe word \033[1;31m'" << search 
-        << "'\033[0m has been found \033[1;33m" << countwords << "\033[0m times.\n";
+      temp_fileout << textTotal;
+      cout << "\n\nThe word \033[1;31m'" << search 
+      << "'\033[0m has been found \033[1;33m" << countwords << "\033[0m times.\n";
+      printf("\n--end of child processing--\n");
+      exit(EXIT_SUCCESS);
     }
+    
     else if(pid < 0)
     {
-        // fork failed
-        cout << "Fork failed\n";
-        return 1;
+      // fork failed
+      cout << "Fork failed\n";
+      return 1;
     }
-    
-    // wait for child and parent to finish
-    wait(NULL);
-    wait(NULL);
-    
-    
-    printf("\n--end of processing--\n");
-    
-  }
-    // wait for child to finish
+ 
+    // wait till child is done and died :'(
     while ((wpid = wait(&status)) > 0);
-    return 0;
+    printf("\n--end of processing--\n");
+
+  }
+  
+  return 0;
 }
 
 
